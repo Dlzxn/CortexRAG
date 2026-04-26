631370d8d6163adc0ad9c72f21f9dc8902ff98fc
Alexey PC <aleshamarichev09@gmail.com>
2026-04-26 19:35:23 +0300

    llm builder

--- src/agents/llm_builder.py
+++ src/agents/llm_builder.py
@@ -1,11 +1,18 @@
 from langchain_core.language_models.chat_models import BaseChatModel
+from langgraph.types import Send
+
+
+from src.state import Topik, WorkerInput, TopikList, ResearchData
+from src.tools import create_md
+
+
 
 class LLM:
     def __init__(self, model: BaseChatModel):
         self.model = model
 
     def __call__(self, message):
-        return self.model.invoke(message)
+        return self.model.invoke(message).content
 
 
 class ModelFactory:
@@ -17,13 +24,15 @@ class ModelFactory:
 
 
 class Manager:
-    def __init__(self, models: tuple[BaseChatModel]):
+    def __init__(self, models: tuple[BaseChatModel], topic_prompt):
         factory = ModelFactory()
         self.models: list[LLM] = factory(models)
         self.action: dict = {}
         self.iter = 0
+        self.topic_prompt = topic_prompt
 
         self._distribution_role()
+
 
     def _distribution_role(self) -> dict:
         if len(self.models) == 0:
@@ -43,15 +52,40 @@ class Manager:
         del self.models
 
 
-    def topic(self, message: str):
-        return self.action['topic'](message)
+    def topic(self, call_num: int):
+        def topic_first(state: Topik):
+            topics = self.action['topic'](self.topic_prompt + state.input).split(',')
+            return {'main_topics': topics}
+
+        def topic(state: WorkerInput):
+            print(state)
+            return {'current_topics': [self.action['topic'](self.topic_prompt + state.get('topic')).split(',')]}
+
+        if call_num == 1:
+            return topic_first
+        else:
+            return topic
 
 
-    def research(self, message: str):
-        if self.iter - 1 == len(self.action['research']):
-            response = self.action['research'][iter](message)
+    def research(self, state: ResearchData):
+        input_topic = state.get('topic')
+        name = state.get('name_dir')
+        if self.iter == len(self.action['research']) - 1:
+            response = self.action['research'][self.iter](input_topic)
             self.iter == 0
         else:
-            response = self.action['research'][iter](message)
+            response = self.action['research'][self.iter](input_topic)
             self.iter += 1
-        return response
+        create_md(f'{name}/{input_topic}.md', response)
+
+
+    @staticmethod
+    def route_llm(state: Topik) -> list:
+        return [Send('retopic', {'topic': x}) for x in state.main_topics]
+
+    @staticmethod
+    def start_research(state: Topik) -> list:
+        return [Send('research', {'topic': x,
+                                          'name_dir': state.main_topics[i]
+                                          }) for i in range(len(state.current_topics)) for x in state.current_topics[i]]
+
