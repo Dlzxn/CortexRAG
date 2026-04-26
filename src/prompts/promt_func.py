9bc001ebe60224228e7c55448d03ff67115a2734
Alexey PC <aleshamarichev09@gmail.com>
2026-04-26 19:36:09 +0300

    fix prompts

--- src/prompts/promt_func.py
+++ src/prompts/promt_func.py
@@ -1,10 +1,10 @@
 from src.prompts.base_prompts import EN_TOPIK_PROMPT, RU_TOPIK_PROMPT
 
 
-def get_topik_prompt(lang: str):
+def get_topik_prompt(lang: str, topic: str):
     if lang == 'en':
-        return EN_TOPIK_PROMPT
+        return EN_TOPIK_PROMPT + f'topic: {topic}'
     elif lang == 'ru':
-        return RU_TOPIK_PROMPT
+        return RU_TOPIK_PROMPT + f'Тема: {topic}'
     else:
         raise ValueError('Language not supported, ru, en modes available')
