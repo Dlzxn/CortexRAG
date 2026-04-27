35725b9527ba5ff5789662c4705130ba39b56668
Alexey PC <aleshamarichev09@gmail.com>
2026-04-27 14:33:59 +0300

    new tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,10 +1,49 @@
-from src.cortexrag import Engine
-from tests.make_llms import LLM_topic, LLM_research
+import os
+import shutil
+from pathlib import Path
+
+import pytest
+from cortexrag import Engine
+from cortexrag.graph import TopicsGraph
+from tests.make_llms import Model
 
 
-def test_topics():
-    model = LLM_topic()
-    model_research = LLM_research()
-    topic = 'weather'
-    engine = Engine(topic, (model, model_research))
+@pytest.fixture
+def temp_dir():
+    temp_dir = Path("temp_test_dir")
+    if temp_dir.exists():
+        shutil.rmtree(temp_dir)
+    temp_dir.mkdir()
+    os.chdir(temp_dir)
+    yield
+    os.chdir("..")
+    shutil.rmtree(temp_dir)
+
+
+@pytest.fixture
+def models():
+    return Model(), Model()
+
+
+def test_engine_with_string_topic(models, temp_dir):
+    topic = "weather"
+    engine = Engine(topic, models)
+    assert engine.topic == [topic]
+    assert len(engine.topic_graphs) == 1
+    assert isinstance(engine.topic_graphs[0], TopicsGraph)
     engine.build()
+
+
+def test_engine_with_iterable_topic(models, temp_dir):
+    topic = ("weather", "animals")
+    engine = Engine(topic, models)
+
+    assert engine.topic == tuple(topic)
+    assert len(engine.topic_graphs) == 2
+    assert all(isinstance(g, TopicsGraph) for g in engine.topic_graphs)
+    engine.build()
+
+
+def test_engine_invalid_topic(models):
+    with pytest.raises(ValueError):
+        Engine(123, models)
